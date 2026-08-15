d71f8fdfb3bf817b65b22b4ab217cb05c3f94739
xiaoxiaoxiaoHuanGe <nhpanou@163.com>
2026-08-15 08:05:27 +0800

    chore: prepare v1.0.3 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.transdot.transferassistant"
         minSdk = 23
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
